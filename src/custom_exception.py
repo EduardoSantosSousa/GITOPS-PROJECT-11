9ac4e3a569cfd0472c72361dc35093791e9edc63
Eduardo Sousa <eduardosousa.eds@gmail.com>
2025-07-23 23:51:16 -0300

    First Commit

---
+++ src/custom_exception.py
@@ -0,0 +1,26 @@
+import traceback
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, erro_datail:sys):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(error_message, erro_datail)
+
+
+    @staticmethod
+    def get_detailed_error_message(erro_message, erro_detail:sys):
+
+        #Getting the exception type, value and traceback
+        _,_, exc_tb = traceback.sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        #capture the full traceback
+        tb_str = "". join(traceback.format_exception(*sys.exc_info()))
+
+       #Including the full traceback in the message
+        return f"Error in {file_name}, line {line_number}: {erro_message} \n\n Original Traceback:\n{tb_str}"
+
+    def __str__(self):
+        return self.error_message
